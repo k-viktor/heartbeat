a8acaf3ebeb97bc5ddf4b887ff6e8ff970f6c5ad
Viktor Kubinec <kubinecviktor@gmail.com>
2017-01-15 11:10:54 +0100

    making the record heartbeat to write to dynamo

--- recordHeartbeat/recordHeartbeat.js
+++ recordHeartbeat/recordHeartbeat.js
@@ -1,12 +1,32 @@
-'use strict';
+use strict';
 
 console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('value1 =', event.key1);
-    console.log('value2 =', event.key2);
-    console.log('value3 =', event.key3);
-    callback(null, event.key1);  // Echo back the first key value
-    //callback('Something went wrong');
+    console.log('Received event:', JSON.stringify(event));
+    
+    var AWS = require("aws-sdk");
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    
+    var params = {
+        TableName: "heartbeats",
+        Item: {
+            "user":  event.user,
+            "id": event.id,
+            "info":  event.info
+        }
+    };
+    
+    docClient.put(params, function(err, data) {
+       if (err) {
+           var errorMsg = 'Unable to record heardbeat' + JSON.stringify(event);
+           console.error(errorMsg);
+           console.error(JSON.stringify(err))
+           callback(errorMsg);
+       } else {
+           console.log("Succeeded:", JSON.stringify(event));
+           callback(null, 'OK');
+       }
+    });
 };
+
